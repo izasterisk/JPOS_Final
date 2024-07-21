045595572280001e895bb9effee3abe16b20040e
izasterisk <135491106+izasterisk@users.noreply.github.com>
2024-07-21 21:31:47 +0700

    Delete JPOS_FE-main directory

--- JPOS_FE-main/components/form.tsx
+++
@@ -1,9 +0,0 @@
-const MyForm = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className="flex size-full flex-col flex-nowrap gap-9">
-            <form />
-        </div>
-    );
-};
-
-export default MyForm;
